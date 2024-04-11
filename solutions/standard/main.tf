c9b6af7aaa731adef0b201942667f49e00ed30fe
Jordan-Williams2 <Jordan.Williams2@ibm.com>
2024-04-11 14:19:50 +0100

    fix: update diagram

--- solutions/standard/main.tf
+++ solutions/standard/main.tf
@@ -78,5 +78,5 @@ module "iam_secrets_engine" {
   region               = var.region
   iam_engine_name      = var.iam_engine_name
   secrets_manager_guid = module.secrets_manager.secrets_manager_guid
-  endpoint_type        = var.service_endpoints == "private" ? var.service_endpoints : "public"
+  endpoint_type        = var.allowed_network == "private-only" ? "private" : "public"
 }
