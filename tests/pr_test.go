08a75d6e2b72988c19b5dbdce57aee8a4d550db2
akocbek@ie.ibm.com <akocbek@ie.ibm.com>
2024-04-26 15:48:47 +0100

    fix tests

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -4,6 +4,7 @@ package test
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"strings"
 	"testing"
@@ -25,13 +26,20 @@ const completeExampleTerraformDir = "examples/complete"
 const fscloudExampleTerraformDir = "examples/fscloud"
 const solutionsTerraformDir = "solutions/standard"
 
-const bestRegionYAMLPath = "../common-dev-assets/common-go-assets/cloudinfo-region-secmgr-prefs.yaml"
 const resourceGroup = "geretain-test-scc-module"
 
 // Define a struct with fields that match the structure of the YAML data
 const yamlLocation = "../common-dev-assets/common-go-assets/common-permanent-resources.yaml"
 
 var permanentResources map[string]interface{}
+
+// Current supported Event Notification regions
+var validRegions = []string{
+	"us-south",
+	"eu-de",
+	"eu-gb",
+	"au-syd",
+}
 
 // TestMain will be run before any parallel tests, used to read data from yaml for use with tests
 func TestMain(m *testing.M) {
@@ -50,12 +58,12 @@ func setupOptions(t *testing.T, prefix string) *testhelper.TestOptions {
 		Testing:      t,
 		TerraformDir: completeExampleTerraformDir,
 		Prefix:       prefix,
+		Region:       validRegions[rand.Intn(len(validRegions))],
 		/*
 		 Comment out the 'ResourceGroup' input to force this tests to create a unique resource group. This is because
 		 there is a restriction with the Event Notification service, which allows only one Lite plan instance per resource group.
 		*/
 		// ResourceGroup:      resourceGroup,
-		BestRegionYAMLPath: bestRegionYAMLPath,
 	})
 
 	return options
@@ -94,7 +102,6 @@ func TestFSCloudInSchematics(t *testing.T) {
 			fscloudExampleTerraformDir + "/*.tf",
 			"modules/fscloud/*.tf",
 		},
-		BestRegionYAMLPath: bestRegionYAMLPath,
 		// ResourceGroup:          resourceGroup,
 		TemplateFolder:         fscloudExampleTerraformDir,
 		Tags:                   []string{"test-schematic"},
@@ -104,7 +111,7 @@ func TestFSCloudInSchematics(t *testing.T) {
 
 	options.TerraformVars = []testschematic.TestSchematicTerraformVar{
 		{Name: "ibmcloud_api_key", Value: options.RequiredEnvironmentVars["TF_VAR_ibmcloud_api_key"], DataType: "string", Secure: true},
-		{Name: "region", Value: options.Region, DataType: "string"},
+		{Name: "region", Value: validRegions[rand.Intn(len(validRegions))], DataType: "string"},
 		{Name: "prefix", Value: options.Prefix, DataType: "string"},
 		{Name: "existing_kms_instance_guid", Value: permanentResources["hpcs_south"], DataType: "string"},
 		{Name: "kms_key_crn", Value: permanentResources["hpcs_south_root_key_crn"], DataType: "string"},
@@ -134,13 +141,12 @@ func TestRunDASolutionSchematics(t *testing.T) {
 		Tags:                   []string{"test-schematic"},
 		DeleteWorkspaceOnFail:  false,
 		WaitJobCompleteMinutes: 60,
-		BestRegionYAMLPath:     bestRegionYAMLPath,
 	})
 
 	options.TerraformVars = []testschematic.TestSchematicTerraformVar{
 		{Name: "ibmcloud_api_key", Value: options.RequiredEnvironmentVars["TF_VAR_ibmcloud_api_key"], DataType: "string", Secure: true},
 		{Name: "prefix", Value: options.Prefix, DataType: "string"},
-		{Name: "region", Value: options.Region, DataType: "string"},
+		{Name: "region", Value: validRegions[rand.Intn(len(validRegions))], DataType: "string"},
 		{Name: "resource_group_name", Value: options.Prefix, DataType: "string"},
 		{Name: "service_plan", Value: "trial", DataType: "string"},
 		{Name: "allowed_network", Value: "private-only", DataType: "string"},
@@ -184,15 +190,6 @@ func GetSecretsManagerKey(sm_id string, sm_region string, sm_key_id string) *str
 func TestRunExistingResourcesInstances(t *testing.T) {
 	t.Parallel()
 
-	// Init test options for DA to get the region, which is used for provisioning the existing resources
-	options := testhelper.TestOptionsDefault(&testhelper.TestOptions{
-		Testing:      t,
-		TerraformDir: solutionsTerraformDir,
-		// Do not hard fail the test if the implicit destroy steps fail to allow a full destroy of resource to occur
-		ImplicitRequired:   false,
-		BestRegionYAMLPath: bestRegionYAMLPath,
-	})
-
 	// ------------------------------------------------------------------------------------
 	// Provision Event Notification, KMS key and resource group first
 	// ------------------------------------------------------------------------------------
@@ -212,7 +209,7 @@ func TestRunExistingResourcesInstances(t *testing.T) {
 		TerraformDir: tempTerraformDir,
 		Vars: map[string]interface{}{
 			"prefix":        prefix,
-			"region":        options.Region,
+			"region":        validRegions[rand.Intn(len(validRegions))],
 			"resource_tags": tags,
 		},
 		// Set Upgrade to true to ensure latest version of providers and modules are used by terratest.
@@ -225,20 +222,25 @@ func TestRunExistingResourcesInstances(t *testing.T) {
 	if existErr != nil {
 		assert.True(t, existErr == nil, "Init and Apply of temp existing resource failed")
 	} else {
-		// add existing resources to previously created options
-		options.TerraformVars = map[string]interface{}{
-			"ibmcloud_api_key":                         os.Getenv("TF_VAR_ibmcloud_api_key"),
-			"region":                                   options.Region,
-			"resource_group_name":                      terraform.Output(t, existingTerraformOptions, "resource_group_name"),
-			"use_existing_resource_group":              true,
-			"existing_event_notification_instance_crn": terraform.Output(t, existingTerraformOptions, "event_notification_instance_crn"),
-			"existing_secrets_manager_kms_key_crn":     terraform.Output(t, existingTerraformOptions, "secrets_manager_kms_key_crn"),
-			"existing_kms_instance_crn":                terraform.Output(t, existingTerraformOptions, "secrets_manager_kms_instance_crn"),
-			"service_plan":                             "trial",
-			"existing_secrets_manager_crn":             terraform.Output(t, existingTerraformOptions, "secrets_manager_instance_crn"),
-			"iam_engine_enabled":                       true,
-			"private_engine_enabled":                   true,
-		}
+		options := testhelper.TestOptionsDefault(&testhelper.TestOptions{
+			Testing:      t,
+			TerraformDir: solutionsTerraformDir,
+			// Do not hard fail the test if the implicit destroy steps fail to allow a full destroy of resource to occur
+			ImplicitRequired: false,
+			TerraformVars: map[string]interface{}{
+				"ibmcloud_api_key":                         os.Getenv("TF_VAR_ibmcloud_api_key"),
+				"region":                                   validRegions[rand.Intn(len(validRegions))],
+				"resource_group_name":                      terraform.Output(t, existingTerraformOptions, "resource_group_name"),
+				"use_existing_resource_group":              true,
+				"existing_event_notification_instance_crn": terraform.Output(t, existingTerraformOptions, "event_notification_instance_crn"),
+				"existing_secrets_manager_kms_key_crn":     terraform.Output(t, existingTerraformOptions, "secrets_manager_kms_key_crn"),
+				"existing_kms_instance_crn":                terraform.Output(t, existingTerraformOptions, "secrets_manager_kms_instance_crn"),
+				"service_plan":                             "trial",
+				"existing_secrets_manager_crn":             terraform.Output(t, existingTerraformOptions, "secrets_manager_instance_crn"),
+				"iam_engine_enabled":                       true,
+				"private_engine_enabled":                   true,
+			},
+		})
 
 		output, err := options.RunTestConsistency()
 		assert.Nil(t, err, "This should not have errored")
