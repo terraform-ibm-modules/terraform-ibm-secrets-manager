03d4d74aad704283923dea75c59266d011542d11
akocbek@ie.ibm.com <akocbek@ie.ibm.com>
2024-04-29 15:18:35 +0100

    fix: do not create kms key if existing SM instance is passed

--- solutions/standard/main.tf
+++ solutions/standard/main.tf
@@ -140,4 +140,3 @@ module "private_secret_engine" {
   certificate_template_name = var.certificate_template_name
   endpoint_type             = var.allowed_network == "private-only" ? "private" : "public"
 }
-
