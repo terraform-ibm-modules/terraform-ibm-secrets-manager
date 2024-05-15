53f6cdd6e72ffa3a07b2195178be2b1ae43b000a
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2024-05-15 16:59:29 +0100

    fix: updated DA variable description (#124)

--- solutions/standard/variables.tf
+++ solutions/standard/variables.tf
@@ -237,6 +237,6 @@ variable "existing_event_notification_instance_crn" {
 
 variable "skip_event_notification_iam_authorization_policy" {
   type        = bool
-  description = "Set to true to skip the creation of an IAM authorization policy that permits all Secrets Manager instances (scoped to the resource group) an 'Event Source Manager' role to the given Event Notifications instance passed in the `existing_en_instance_crn` input variable."
+  description = "Set to true to skip the creation of an IAM authorization policy that permits all Secrets Manager instances (scoped to the resource group) an 'Event Source Manager' role to the given Event Notifications instance passed in the `existing_event_notification_instance_crn` input variable."
   default     = false
 }
