413baf0914f8766464a4998ab6932ef8469da6cd
Jordan-Williams2 <Jordan.Williams2@ibm.com>
2024-06-24 17:34:18 +0100

    fix: broken topic event notifications

--- solutions/standard/main.tf
+++ solutions/standard/main.tf
@@ -34,7 +34,7 @@ module "kms" {
   }
   count                       = var.existing_secrets_manager_crn != null || var.existing_secrets_manager_kms_key_crn != null ? 0 : 1 # no need to create any KMS resources if passing an existing key, or bucket
   source                      = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                     = "4.13.4"
+  version                     = "4.13.2"
   create_key_protect_instance = false
   region                      = local.kms_region
   existing_kms_instance_guid  = local.existing_kms_guid
@@ -163,8 +163,15 @@ data "ibm_en_destinations" "en_destinations" {
   instance_guid = local.existing_en_guid
 }
 
+resource "time_sleep" "wait_for_secrets_manager" {
+  depends_on = [module.secrets_manager]
+
+  create_duration = "30s"
+}
+
 resource "ibm_en_topic" "en_topic" {
   count         = var.existing_event_notification_instance_crn != null ? 1 : 0
+  depends_on    = [time_sleep.wait_for_secrets_manager]
   instance_guid = local.existing_en_guid
   name          = "Secrets Manager Topic"
   description   = "Topic for Secrets Manager events routing"
