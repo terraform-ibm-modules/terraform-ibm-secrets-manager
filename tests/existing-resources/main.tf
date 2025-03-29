d88416647c9f36d39793b65bcc58cb8f7882c456
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-03-29 07:19:49 +0000

    fix(deps): update terraform-module

--- tests/existing-resources/main.tf
+++ tests/existing-resources/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "event_notifications" {
   source            = "terraform-ibm-modules/event-notifications/ibm"
-  version           = "1.19.4"
+  version           = "1.19.6"
   resource_group_id = module.resource_group.resource_group_id
   name              = "${var.prefix}-en"
   tags              = var.resource_tags
