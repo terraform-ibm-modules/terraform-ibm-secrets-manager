6675e15baed2da887ff6bf3aefa6a81cd789bf29
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-04-27 18:56:51 +0000

    fix(deps): update terraform-module

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -27,7 +27,7 @@ module "key_protect" {
 
 module "event_notification" {
   source            = "terraform-ibm-modules/event-notifications/ibm"
-  version           = "1.19.20"
+  version           = "1.19.21"
   resource_group_id = module.resource_group.resource_group_id
   name              = "${var.prefix}-en"
   tags              = var.resource_tags
