b7a10a6135e325f78e7cd2b467392afb787a48b8
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-05-18 14:55:18 +0000

    fix(deps): update terraform dependencies

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -8,7 +8,7 @@ module "resource_group" {
 
 module "key_protect" {
   source                    = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                   = "5.1.4"
+  version                   = "5.1.5"
   key_protect_instance_name = "${var.prefix}-key-protect"
   resource_group_id         = module.resource_group.resource_group_id
   region                    = var.region
@@ -27,7 +27,7 @@ module "key_protect" {
 
 module "event_notification" {
   source            = "terraform-ibm-modules/event-notifications/ibm"
-  version           = "2.2.2"
+  version           = "2.2.4"
   resource_group_id = module.resource_group.resource_group_id
   name              = "${var.prefix}-en"
   tags              = var.resource_tags
