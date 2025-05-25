0ffc11f85caab76c1f08d90d1ebd761cdfc9b687
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-05-25 17:24:00 +0100

    fix(deps): update terraform dependencies (#340)

--- examples/fscloud/main.tf
+++ examples/fscloud/main.tf
@@ -42,7 +42,7 @@ module "cbr_zone" {
 
 module "event_notification" {
   source            = "terraform-ibm-modules/event-notifications/ibm"
-  version           = "2.2.4"
+  version           = "2.3.5"
   resource_group_id = module.resource_group.resource_group_id
   name              = "${var.prefix}-en"
   tags              = var.resource_tags
