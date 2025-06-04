dca7884ad56f1ae9be34142008047c4ed6c7b0b3
Rajat Agrawal <rajatatibm@gmail.com>
2025-06-04 15:13:46 +0100

    provider visibility

--- solutions/fully-configurable/provider.tf
+++ solutions/fully-configurable/provider.tf
@@ -2,12 +2,12 @@ provider "ibm" {
   ibmcloud_api_key      = var.ibmcloud_api_key
   region                = var.region
   visibility            = var.provider_visibility
-  private_endpoint_type = var.region == "ca-mon" ? "vpe" : null
+  private_endpoint_type = (var.provider_visibility == "private" && var.region == "ca-mon") ? "vpe" : null
 }
 provider "ibm" {
   alias                 = "kms"
   ibmcloud_api_key      = var.ibmcloud_kms_api_key != null ? var.ibmcloud_kms_api_key : var.ibmcloud_api_key
   region                = local.kms_region
   visibility            = var.provider_visibility
-  private_endpoint_type = var.region == "ca-mon" ? "vpe" : null
+  private_endpoint_type = (var.provider_visibility == "private" && var.region == "ca-mon") ? "vpe" : null
 }
