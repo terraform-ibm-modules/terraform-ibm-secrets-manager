08291c65329178920d0a090dfe70e5d4c22b0807
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2025-06-20 12:35:20 +0100

    fix: add fix for KMS cross account bug

--- solutions/fully-configurable/main.tf
+++ solutions/fully-configurable/main.tf
@@ -61,7 +61,7 @@ resource "ibm_iam_authorization_policy" "kms_policy" {
   provider                 = ibm.kms
   source_service_account   = data.ibm_iam_account_settings.iam_account_settings[0].account_id
   source_service_name      = "secrets-manager"
-  source_resource_group_id = module.resource_group[0].resource_group_id
+  source_resource_group_id = module.resource_group.resource_group_id
   roles                    = ["Reader"]
   description              = "Allow all Secrets Manager instances in the resource group ${local.kms_account_id} to read the ${local.kms_service_name} key ${local.kms_key_id} from the instance GUID ${local.kms_instance_guid}"
   resource_attributes {
@@ -109,11 +109,11 @@ resource "ibm_iam_authorization_policy" "secrets_manager_hpcs_policy" {
   provider                    = ibm.kms
   source_service_account      = data.ibm_iam_account_settings.iam_account_settings[0].account_id
   source_service_name         = "secrets-manager"
-  source_resource_group_id    = module.resource_group[0].resource_group_id
+  source_resource_group_id    = module.resource_group.resource_group_id
   target_service_name         = local.kms_service_name
   target_resource_instance_id = local.kms_instance_guid
   roles                       = ["Viewer"]
-  description                 = "Allow all Secrets Manager instances in the resource group ${module.resource_group[0].resource_group_id} in the account ${local.kms_account_id} to view from the ${local.kms_service_name} instance GUID ${local.kms_instance_guid}"
+  description                 = "Allow all Secrets Manager instances in the resource group ${module.resource_group.resource_group_id} in the account ${local.kms_account_id} to view from the ${local.kms_service_name} instance GUID ${local.kms_instance_guid}"
 }
 
 # workaround for https://github.com/IBM-Cloud/terraform-provider-ibm/issues/4478
