806f4694d17cccfb016dd86691a2893783b2beae
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-07-05 20:07:19 +0000

    fix(deps): update terraform dependencies

--- examples/fscloud/main.tf
+++ examples/fscloud/main.tf
@@ -3,7 +3,7 @@
 ##############################################################################
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.2.0"
+  version = "1.2.1"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
@@ -30,7 +30,7 @@ resource "ibm_is_vpc" "vpc" {
 ##############################################################################
 module "cbr_zone" {
   source           = "terraform-ibm-modules/cbr/ibm//modules/cbr-zone-module"
-  version          = "1.31.0"
+  version          = "1.32.2"
   name             = "${var.prefix}-CBR-zone"
   zone_description = "CBR Network zone representing VPC"
   account_id       = data.ibm_iam_account_settings.iam_account_settings.account_id
@@ -42,7 +42,7 @@ module "cbr_zone" {
 
 module "event_notification" {
   source            = "terraform-ibm-modules/event-notifications/ibm"
-  version           = "2.3.12"
+  version           = "2.4.9"
   resource_group_id = module.resource_group.resource_group_id
   name              = "${var.prefix}-en"
   tags              = var.resource_tags
@@ -57,7 +57,7 @@ module "event_notification" {
 
 module "kms_key_crn_parser" {
   source  = "terraform-ibm-modules/common-utilities/ibm//modules/crn-parser"
-  version = "1.1.0"
+  version = "1.2.0"
   crn     = var.kms_key_crn
 }
 
