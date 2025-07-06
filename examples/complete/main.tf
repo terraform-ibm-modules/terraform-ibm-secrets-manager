420ee117116b5fd09ab7084951115d747838ac57
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-07-06 20:15:21 +0100

    fix(deps): update terraform dependencies (#360)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -27,7 +27,7 @@ module "key_protect" {
 
 module "event_notification" {
   source            = "terraform-ibm-modules/event-notifications/ibm"
-  version           = "2.4.9"
+  version           = "2.4.10"
   resource_group_id = module.resource_group.resource_group_id
   name              = "${var.prefix}-en"
   tags              = var.resource_tags
