c017d6ed977f2d420aed1a698174850f7250317b
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-07-07 06:43:30 +0000

    fix(deps): update terraform dependencies

--- tests/new-resources/main.tf
+++ tests/new-resources/main.tf
@@ -17,7 +17,7 @@ module "resource_group" {
 module "secrets_manager" {
   count                         = var.provision_secrets_manager == true ? 1 : 0
   source                        = "terraform-ibm-modules/secrets-manager/ibm"
-  version                       = "2.6.6" # Replace "X.X.X" with a release version to lock into a specific release
+  version                       = "2.6.7" # Replace "X.X.X" with a release version to lock into a specific release
   resource_group_id             = module.resource_group.resource_group_id
   region                        = var.region
   secrets_manager_name          = "${var.prefix}-tsm"
