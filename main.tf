37290f7c5ee0872dae63bf3c1a952f6080e6bd34
Jordan <jordanwilliams997@hotmail.com>
2025-07-16 18:26:01 +0100

    fix: bug with KMS key crn parser (#363)

--- main.tf
+++ main.tf
@@ -79,10 +79,10 @@ locals {
 ########################################################################################################################
 
 module "kms_key_crn_parser" {
-  count   = local.create_kms_auth_policy ? 1 : 0
+  count   = var.kms_encryption_enabled ? 1 : 0
   source  = "terraform-ibm-modules/common-utilities/ibm//modules/crn-parser"
   version = "1.2.0"
-  crn     = var.kms_key_crn
+  crn     = var.kms_key_crn != null ? var.kms_key_crn : ""
 }
 
 # Create auth policy (scoped to exact KMS key)
