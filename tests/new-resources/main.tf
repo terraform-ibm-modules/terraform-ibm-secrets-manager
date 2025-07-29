2a22d376bb0231b8af615f82b7621c8ab6c9631f
shemau <shemau@gmail.com>
2025-07-29 16:18:50 +0100

    fix: consume current version of module

--- tests/new-resources/main.tf
+++ tests/new-resources/main.tf
@@ -16,8 +16,7 @@ module "resource_group" {
 
 module "secrets_manager" {
   count                         = var.provision_secrets_manager == true ? 1 : 0
-  source                        = "terraform-ibm-modules/secrets-manager/ibm"
-  version                       = "2.6.15" # Replace "X.X.X" with a release version to lock into a specific release
+  source                        = "../.."
   resource_group_id             = module.resource_group.resource_group_id
   region                        = var.region
   secrets_manager_name          = "${var.prefix}-tsm"
