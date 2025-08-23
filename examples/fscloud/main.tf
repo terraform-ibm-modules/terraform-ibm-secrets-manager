1335baeb8021b030013206ed304ecf5bb506a900
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-08-23 07:03:57 +0000

    chore(deps): update dependencies

--- examples/fscloud/main.tf
+++ examples/fscloud/main.tf
@@ -42,7 +42,7 @@ module "cbr_zone" {
 
 module "event_notification" {
   source            = "terraform-ibm-modules/event-notifications/ibm"
-  version           = "2.6.14"
+  version           = "2.6.15"
   resource_group_id = module.resource_group.resource_group_id
   name              = "${var.prefix}-en"
   tags              = var.resource_tags
