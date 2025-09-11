911abcf9016ec6fa76ecf53f98c22240b05503d0
mukul-palit <mukul.palit@ibm.com>
2025-09-11 11:26:28 +0530

    PR changes

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -446,7 +446,7 @@ func TestSecretsManagerDefaultConfiguration(t *testing.T) {
 
 // TestDependencyPermutations runs dependency permutations for the Secrets Manager and all its dependencies
 func TestDependencyPermutations(t *testing.T) {
-	t.Skip() // skipping permutations test untill we do a refactor
+	t.Skip() // skipping permutations test until we do a refactor
 
 	options := testaddons.TestAddonsOptionsDefault(&testaddons.TestAddonOptions{
 		Testing: t,
