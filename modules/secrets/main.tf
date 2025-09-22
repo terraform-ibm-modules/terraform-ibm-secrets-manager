b7051056038aa7c34ad5a8a9db2905cf4cb1b9a5
Aditya-ranjan-16 <aditya16ranjan@gmail.com>
2025-09-23 01:18:45 +0530

    revert last changes

--- modules/secrets/main.tf
+++ modules/secrets/main.tf
@@ -7,7 +7,6 @@ locals {
     for secret_group in var.secrets :
     secret_group.existing_secret_group ? [] : [{
       secret_group_name                = secret_group.secret_group_name
-      secret_group_id                  = secret_group.secret_group_id
       secret_group_description         = secret_group.secret_group_description
       secret_group_create_access_group = secret_group.create_access_group
       secret_group_access_group_name   = secret_group.access_group_name
@@ -47,7 +46,7 @@ locals {
     for secret_group in var.secrets :
     secret_group.existing_secret_group ? [
       for secret in secret_group.secrets : merge({
-        secret_group_id = secret_group.secret_group_id!="" ? secret_group.secret_group_id : data.ibm_sm_secret_groups.existing_secret_groups.secret_groups[index(data.ibm_sm_secret_groups.existing_secret_groups.secret_groups[*].name, secret_group.secret_group_name)].id
+        secret_group_id = data.ibm_sm_secret_groups.existing_secret_groups.secret_groups[index(data.ibm_sm_secret_groups.existing_secret_groups.secret_groups[*].name, secret_group.secret_group_name)].id
       }, secret)
       ] : [
       for secret in secret_group.secrets : merge({
