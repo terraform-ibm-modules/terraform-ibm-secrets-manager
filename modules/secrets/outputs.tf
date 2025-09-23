80288836da05ad2789ae1c9336ff5fcef309ba20
Aditya-ranjan-16 <aditya16ranjan@gmail.com>
2025-09-23 15:16:06 +0530

    fix

--- modules/secrets/outputs.tf
+++ modules/secrets/outputs.tf
@@ -2,12 +2,12 @@
 # Outputs
 ##############################################################################
 
-output "secret_groups" {
-  description = "IDs of the created Secret Group"
-  value       = module.secret_groups
-}
+# output "secret_groups" {
+#   description = "IDs of the created Secret Group"
+#   value       = module.secret_groups
+# }
 
-output "secrets" {
-  description = "List of secret manager secret config data"
-  value       = module.secrets
-}
+# output "secrets" {
+#   description = "List of secret manager secret config data"
+#   value       = module.secrets
+# }
