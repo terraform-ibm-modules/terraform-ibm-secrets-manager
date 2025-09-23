78dcd39bcbc814021332c718995e0ac2a5271512
Aditya-ranjan-16 <aditya16ranjan@gmail.com>
2025-09-23 15:23:26 +0530

    test6

--- modules/secrets/main.tf
+++ modules/secrets/main.tf
@@ -1,11 +1,40 @@
 ##############################################################################
-# Existing Secret Groups (data lookup only)
+# Secret Group
 ##############################################################################
+
+locals {
+  secret_groups = flatten([
+    for secret_group in var.secrets :
+    secret_group.existing_secret_group ? [] : [{
+      secret_group_name                = secret_group.secret_group_name
+      secret_group_description         = secret_group.secret_group_description
+      secret_group_create_access_group = secret_group.create_access_group
+      secret_group_access_group_name   = secret_group.access_group_name
+      secret_group_access_group_roles  = secret_group.access_group_roles
+      secret_group_access_group_tags   = secret_group.access_group_tags
+    }]
+  ])
+}
 
 data "ibm_sm_secret_groups" "existing_secret_groups" {
   instance_id   = var.existing_sm_instance_guid
   region        = var.existing_sm_instance_region
   endpoint_type = var.endpoint_type
+}
+
+module "secret_groups" {
+  for_each                 = { for obj in local.secret_groups : obj.secret_group_name => obj }
+  source                   = "terraform-ibm-modules/secrets-manager-secret-group/ibm"
+  version                  = "1.3.15"
+  region                   = var.existing_sm_instance_region
+  secrets_manager_guid     = var.existing_sm_instance_guid
+  secret_group_name        = each.value.secret_group_name
+  secret_group_description = each.value.secret_group_description
+  endpoint_type            = var.endpoint_type
+  create_access_group      = each.value.secret_group_create_access_group
+  access_group_name        = each.value.secret_group_access_group_name
+  access_group_roles       = each.value.secret_group_access_group_roles
+  access_group_tags        = each.value.secret_group_access_group_tags
 }
 
 ##############################################################################
@@ -27,17 +56,15 @@ locals {
     ]
   ])
 }
-
-# Create secrets in existing groups
+# create secret
 module "secrets" {
-  for_each             = { for obj in local.secrets : obj.secret_name => obj }
-  source               = "terraform-ibm-modules/secrets-manager-secret/ibm"
-  version              = "1.9.0"
-  region               = var.existing_sm_instance_region
-  secrets_manager_guid = var.existing_sm_instance_guid
-  secret_group_id      = each.value.secret_group_id
-  endpoint_type        = var.endpoint_type
-
+  for_each                                    = { for obj in local.secrets : obj.secret_name => obj }
+  source                                      = "terraform-ibm-modules/secrets-manager-secret/ibm"
+  version                                     = "1.9.0"
+  region                                      = var.existing_sm_instance_region
+  secrets_manager_guid                        = var.existing_sm_instance_guid
+  secret_group_id                             = each.value.secret_group_id
+  endpoint_type                               = var.endpoint_type
   secret_name                                 = each.value.secret_name
   secret_description                          = each.value.secret_description
   secret_type                                 = each.value.secret_type
