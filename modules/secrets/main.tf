2fd47dbdaa43b1c185cf98e4ffbcba06b749f9c2
Aditya-ranjan-16 <aditya16ranjan@gmail.com>
2025-09-24 18:40:08 +0530

    test14

--- modules/secrets/main.tf
+++ modules/secrets/main.tf
@@ -1,20 +1,6 @@
 ##############################################################################
-# Secret Group
+# Secret Group (lookup only; no creation)
 ##############################################################################
-
-locals {
-  secret_groups = flatten([
-    for secret_group in var.secrets :
-    secret_group.existing_secret_group ? [] : [{
-      secret_group_name                = secret_group.secret_group_name
-      secret_group_description         = secret_group.secret_group_description
-      secret_group_create_access_group = secret_group.create_access_group
-      secret_group_access_group_name   = secret_group.access_group_name
-      secret_group_access_group_roles  = secret_group.access_group_roles
-      secret_group_access_group_tags   = secret_group.access_group_tags
-    }]
-  ])
-}
 
 data "ibm_sm_secret_groups" "existing_secret_groups" {
   instance_id   = var.existing_sm_instance_guid
@@ -22,47 +8,24 @@ data "ibm_sm_secret_groups" "existing_secret_groups" {
   endpoint_type = var.endpoint_type
 }
 
-module "secret_groups" {
-  for_each                 = { for obj in local.secret_groups : obj.secret_group_name => obj }
-  source                   = "terraform-ibm-modules/secrets-manager-secret-group/ibm"
-  version                  = "1.3.15"
-  region                   = var.existing_sm_instance_region
-  secrets_manager_guid     = var.existing_sm_instance_guid
-  secret_group_name        = each.value.secret_group_name
-  secret_group_description = each.value.secret_group_description
-  endpoint_type            = var.endpoint_type
-  create_access_group      = each.value.secret_group_create_access_group
-  access_group_name        = each.value.secret_group_access_group_name
-  access_group_roles       = each.value.secret_group_access_group_roles
-  access_group_tags        = each.value.secret_group_access_group_tags
-}
-
 ##############################################################################
 # Secrets
 ##############################################################################
 
 locals {
-    # Build a deterministic map of existing secret groups keyed by name
-  existing_groups_by_name = {
-    for g in data.ibm_sm_secret_groups.existing_secret_groups.secret_groups :
-    g.name => g
-  }
+  # We keep the same input structure for var.secrets, including fields like
+  # existing_secret_group, create_access_group, access_group_* etc.,
+  # but we always resolve to an existing group's ID.
   secrets = flatten([
-    for secret_group in var.secrets :
-    secret_group.existing_secret_group ?
-    [
+    for secret_group in var.secrets : [
       for secret in secret_group.secrets : merge(
         {
-          secret_group_id = local.existing_groups_by_name[secret_group.secret_group_name].id
-        },
-        secret
-      )
-    ]
-    :
-    [
-      for secret in secret_group.secrets : merge(
-        {
-          secret_group_id = module.secret_groups[secret_group.secret_group_name].secret_group_id
+          secret_group_id = data.ibm_sm_secret_groups.existing_secret_groups.secret_groups[
+            index(
+              data.ibm_sm_secret_groups.existing_secret_groups.secret_groups[*].name,
+              secret_group.secret_group_name
+            )
+          ].id
         },
         secret
       )
@@ -72,13 +35,16 @@ locals {
 
 # create secret
 module "secrets" {
+  # Interface unchanged
   for_each                                    = { for obj in local.secrets : obj.secret_name => obj }
   source                                      = "terraform-ibm-modules/secrets-manager-secret/ibm"
   version                                     = "1.9.0"
+
   region                                      = var.existing_sm_instance_region
   secrets_manager_guid                        = var.existing_sm_instance_guid
-  secret_group_id                             = each.value.secret_group_id
   endpoint_type                               = var.endpoint_type
+
+  secret_group_id                             = each.value.secret_group_id
   secret_name                                 = each.value.secret_name
   secret_description                          = each.value.secret_description
   secret_type                                 = each.value.secret_type
--- modules/secrets/outputs.tf
+++ modules/secrets/outputs.tf
@@ -2,19 +2,19 @@
 # Outputs
 ##############################################################################
 
-output "secret_groups" {
-  description = "IDs of the created Secret Group"
-  value       = module.secret_groups
-}
+# output "secret_groups" {
+#   description = "IDs of the created Secret Group"
+#   value       = module.secret_groups
+# }
 
 output "secrets" {
   description = "List of secret manager secret config data"
   value       = module.secrets
 }
 
-output "loccal_secret_groups" {
-  value = local.secret_groups
-}
+# output "loccal_secret_groups" {
+#   value = local.secret_groups
+# }
 
 output "loccal_secrets" {
   value = local.secrets
