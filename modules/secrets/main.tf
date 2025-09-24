0c2ce030cc36516dbccb3ae14f79755f27e9ba20
Aditya-ranjan-16 <aditya16ranjan@gmail.com>
2025-09-24 20:44:43 +0530

    test16

--- modules/secrets/main.tf
+++ modules/secrets/main.tf
@@ -1,31 +1,41 @@
-locals {
-  distinct_group_names = toset([for sg in var.secrets : sg.secret_group_name])
-}
-
-# Single global data read
+##############################################################################
+# Lookup all groups once
+##############################################################################
 data "ibm_sm_secret_groups" "all" {
   instance_id   = var.existing_sm_instance_guid
   region        = var.existing_sm_instance_region
   endpoint_type = var.endpoint_type
 }
 
-# Stabilize IDs via null_resource triggers
-resource "null_resource" "group_ids" {
-  for_each = local.distinct_group_names
+locals {
+  # distinct names from inputs; interface unchanged
+  distinct_group_names = toset([for sg in var.secrets : sg.secret_group_name])
 
-  triggers = {
-    name = each.key
-    id   = data.ibm_sm_secret_groups.all.secret_groups[
-      index(data.ibm_sm_secret_groups.all.secret_groups[*].name, each.key)
-    ].id
+  # name -> id resolution from the current data read
+  group_id_by_name_now = {
+    for g in data.ibm_sm_secret_groups.all.secret_groups : g.name => g.id
   }
 }
 
-# Build a stable map from the persisted triggers
+##############################################################################
+# Persist a stable mapping name -> id in state
+##############################################################################
+resource "terraform_data" "group_id" {
+  for_each = local.distinct_group_names
+
+  # The only trigger that should cause recomputation is the name change.
+  # We put the current resolved id in "input" so it gets stored in state on first apply.
+  input = {
+    name = each.key
+    id   = local.group_id_by_name_now[each.key]
+  }
+}
+
 locals {
+  # Stable, plan-known IDs coming from state after first apply
   stable_group_id_by_name = {
-    for name, r in null_resource.group_ids :
-    name => r.triggers.id
+    for name, r in terraform_data.group_id :
+    name => r.output.id
   }
 
   secrets = flatten([
@@ -51,17 +61,17 @@ module "secrets" {
 
   secret_group_id = each.value.secret_group_id
   secret_name     = each.value.secret_name
-  secret_description                          = each.value.secret_description
-  secret_type                                 = each.value.secret_type
-  imported_cert_certificate                   = each.value.imported_cert_certificate
-  imported_cert_private_key                   = each.value.imported_cert_private_key
-  imported_cert_intermediate                  = each.value.imported_cert_intermediate
-  secret_username                             = each.value.secret_username
-  secret_labels                               = each.value.secret_labels
-  secret_payload_password                     = each.value.secret_payload_password
-  secret_auto_rotation                        = each.value.secret_auto_rotation
-  secret_auto_rotation_unit                   = each.value.secret_auto_rotation_unit
-  secret_auto_rotation_interval               = each.value.secret_auto_rotation_interval
+  secret_description = each.value.secret_description
+  secret_type     = each.value.secret_type
+  imported_cert_certificate  = each.value.imported_cert_certificate
+  imported_cert_private_key  = each.value.imported_cert_private_key
+  imported_cert_intermediate = each.value.imported_cert_intermediate
+  secret_username            = each.value.secret_username
+  secret_labels              = each.value.secret_labels
+  secret_payload_password    = each.value.secret_payload_password
+  secret_auto_rotation       = each.value.secret_auto_rotation
+  secret_auto_rotation_unit  = each.value.secret_auto_rotation_unit
+  secret_auto_rotation_interval = each.value.secret_auto_rotation_interval
   service_credentials_ttl                     = each.value.service_credentials_ttl
   service_credentials_source_service_crn      = each.value.service_credentials_source_service_crn
   service_credentials_source_service_role_crn = each.value.service_credentials_source_service_role_crn
