ede612a1a613bac06985c901285229786f12081f
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-01-02 20:10:44 +0000

    fix(deps): update dependencies (#458)

--- modules/secrets/main.tf
+++ modules/secrets/main.tf
@@ -25,7 +25,7 @@ data "ibm_sm_secret_groups" "existing_secret_groups" {
 module "secret_groups" {
   for_each                 = { for obj in local.secret_groups : obj.secret_group_name => obj }
   source                   = "terraform-ibm-modules/secrets-manager-secret-group/ibm"
-  version                  = "1.3.35"
+  version                  = "1.3.36"
   region                   = var.existing_sm_instance_region
   secrets_manager_guid     = var.existing_sm_instance_guid
   secret_group_name        = each.value.secret_group_name
@@ -60,7 +60,7 @@ locals {
 module "secrets" {
   for_each                                    = { for obj in local.secrets : obj.secret_name => obj }
   source                                      = "terraform-ibm-modules/secrets-manager-secret/ibm"
-  version                                     = "1.9.9"
+  version                                     = "1.9.11"
   region                                      = var.existing_sm_instance_region
   secrets_manager_guid                        = var.existing_sm_instance_guid
   secret_group_id                             = each.value.secret_group_id
