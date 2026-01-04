f4838590053f06576361a9c86ce4ef754917c03b
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-01-04 06:19:41 +0000

    chore(deps): update terraform terraform-ibm-modules/event-notifications/ibm to v2.10.34

--- examples/fscloud/main.tf
+++ examples/fscloud/main.tf
@@ -39,7 +39,7 @@ module "cbr_zone_schematics" {
 
 module "event_notification" {
   source            = "terraform-ibm-modules/event-notifications/ibm"
-  version           = "2.10.33"
+  version           = "2.10.34"
   resource_group_id = module.resource_group.resource_group_id
   name              = "${var.prefix}-en"
   tags              = var.resource_tags
