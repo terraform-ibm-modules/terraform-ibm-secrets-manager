8ca71bd13ee5ea3e211a9d185a8a585092125f10
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-01-24 04:19:28 +0000

    fix(deps): update dependencies (#470)

--- main.tf
+++ main.tf
@@ -81,7 +81,7 @@ locals {
 module "kms_key_crn_parser" {
   count   = var.kms_encryption_enabled ? 1 : 0
   source  = "terraform-ibm-modules/common-utilities/ibm//modules/crn-parser"
-  version = "1.3.7"
+  version = "1.4.1"
   crn     = var.kms_key_crn != null ? var.kms_key_crn : ""
 }
 
@@ -163,7 +163,7 @@ locals {
 module "cbr_rule" {
   count            = length(var.cbr_rules) > 0 ? length(var.cbr_rules) : 0
   source           = "terraform-ibm-modules/cbr/ibm//modules/cbr-rule-module"
-  version          = "1.35.10"
+  version          = "1.35.11"
   rule_description = var.cbr_rules[count.index].description
   enforcement_mode = var.cbr_rules[count.index].enforcement_mode
   rule_contexts    = var.cbr_rules[count.index].rule_contexts
