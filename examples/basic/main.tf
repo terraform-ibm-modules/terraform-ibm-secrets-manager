06b9b460a5a7ddb57c99c2afa07d78d8725014be
Arya Girish K <arya.girish.k@ibm.com>
2026-01-27 09:56:48 +0530

    testing

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -7,7 +7,7 @@ module "resource_group" {
 }
 
 module "secrets_manager" {
-  source               = "../.."
+  source               = "terraform-ibm-modules/secrets-manager/ibm"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   secrets_manager_name = "${var.prefix}-secrets-manager" #tfsec:ignore:general-secrets-no-plaintext-exposure
