049e139c55175d80f022d3186a42fe920acd3e21
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-02-21 14:21:53 +0000

    fix(deps): pin dependencies

--- examples/fscloud/main.tf
+++ examples/fscloud/main.tf
@@ -39,7 +39,7 @@ module "cbr_zone_schematics" {
 
 module "event_notification" {
   source            = "terraform-ibm-modules/event-notifications/ibm"
-  version           = "2.11.16"
+  version           = "2.11.17"
   resource_group_id = module.resource_group.resource_group_id
   name              = "${var.prefix}-en"
   tags              = var.resource_tags
