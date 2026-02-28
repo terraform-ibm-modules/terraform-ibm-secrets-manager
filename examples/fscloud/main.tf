abaa98b425a6d2d28110ce9bf286d00d43d89468
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-02-28 15:44:04 +0000

    fix(deps): update dependencies

--- examples/fscloud/main.tf
+++ examples/fscloud/main.tf
@@ -39,7 +39,7 @@ module "cbr_zone_schematics" {
 
 module "event_notification" {
   source            = "terraform-ibm-modules/event-notifications/ibm"
-  version           = "2.11.21"
+  version           = "2.11.22"
   resource_group_id = module.resource_group.resource_group_id
   name              = "${var.prefix}-en"
   tags              = var.resource_tags
